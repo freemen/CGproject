b9b4e24b014c29850c68603fd9fec17d304cebe8
Freemen Liu <freemensmall@gmail.com>
2013-05-09 15:59:54 +0800

    1.finish the Bresenham algorithm, 2.realize the draw lines and draw polygon function with the Bresenham, 3.abstract the draw point function

--- d01/draws.py
+++ d01/draws.py
@@ -21,17 +21,49 @@ class VertexGroup:
             self.lastVertex = []
         self.vertexs.append([x,y])
         return self.lastVertex
+    def close(self):
+        return self.vertexs[-1], self.vertexs[0]
 
 class Bresenham:
     def __init__(self, canvas):
         self.canvas = canvas
         
-
-    def draw(x1, y1, x2, y2):
+    def draw(self, x1, y1, x2, y2):
+        x = x1
+        y = y1
+        self.canvas.drawPoint(x, y)
         dx = x2 - x1
         dy = y2 - y1
-        
-        
+        ddx = abs(dx)
+        ddy = abs(dy)
+        if dx >= 0:
+            sx = 1
+        else:
+            sx = -1
+        if dy >= 0:
+            sy = 1
+        else:
+            sy = -1
+        if ddy <= ddx:
+            wucha = (ddy<<1) - ddx
+            for i in range(0, ddx):
+                x += sx
+                if wucha < 0:
+                    wucha += (ddy<<1)
+                else:
+                    y += sy
+                    wucha += ((ddy<<1) - (ddx<<1))
+                self.canvas.drawPoint(x, y)
+        else:
+            wucha = (ddx<<1) - ddy
+            for i in range(0, ddy):
+                y += sy
+                if wucha <0:
+                    wucha += (ddx<<1)
+                else:
+                    x += sx
+                    wucha += ((ddx<<1) -(ddy<<1))
+                self.canvas.drawPoint(x, y)
     
 class Showing:
     def __init__(self, tk):
@@ -39,8 +71,10 @@ class Showing:
         self.show = Canvas(tk,bg="#fbf4f4",height=500,width=600)
         self.show.grid(row=0, rowspan = 100, column=1)#, columnspan = 2)
         self.show.bind('<Button-1>',self.press)
+        self.show.bind('<Button-3>', self.rightclick)
 
         self.color = "#222"
+        self.pointcolor = "#d44"
         self.fillcolor = "#259"
         self.state = 'nothing'
 
@@ -49,14 +83,19 @@ class Showing:
         
     def press(self, event):
         returnValue = self.functions[self.state](self, event)
+    def rightclick(self, event):
+        returnValue = self.rightfunctions[self.state](self, event)
                 
+    def drawMyPoint(self, x, y, color):
+        self.show.create_line(x, y ,x+1 ,y+1 , fill=color)
     def drawPoint(self, x, y):
-        self.show.create_line(x, y ,x+1 ,y+1 , fill=self.color)
+        self.drawMyPoint(x, y, self.color)        
 
     def doNothing(self, event):
-        for i in range(-4,5):
-            for j in range(-4,5):
-                self.drawPoint(event.x+i, event.y+j)        
+        size = 2
+        for i in range(-size,size+1):
+            for j in range(-size,size+1):
+                self.drawMyPoint(event.x+i, event.y+j, self.pointcolor)        
 
     def drawDot(self, event):
         self.drawPoint(event.x, event.y)
@@ -67,16 +106,32 @@ class Showing:
             self.forLine.draw(firstV[0], firstV[1], event.x, event.y)
             self.vGroup.clear()
 
+    def drawPolygon(self, event):
+        lastV = self.vGroup.addPoint(event.x, event.y)
+        if lastV:
+            self.forLine.draw(lastV[0], lastV[1], event.x, event.y)
+        else:
+            self.doNothing(event)
+    def endPolygon(self):
+        endV, beginV = self.vGroup.close()
+        self.forLine.draw(endV[0], endV[1], beginV[0], beginV[1])
+        self.vGroup.clear()
+    def closePolygon(self, event):
+        self.endPolygon()
+
 
     functions = {
         'nothing': doNothing,
         'dot': drawDot,
         'line': drawLine,
-        'polygon': doNothing,
+        'polygon': drawPolygon,
         'filledpolygon': doNothing,
         'twoDtrans': doNothing,
         'cut': doNothing,
         'animation': doNothing,
+        }
+    rightfunctions = {
+        'polygon': closePolygon,
         }
         
 class Choice:
